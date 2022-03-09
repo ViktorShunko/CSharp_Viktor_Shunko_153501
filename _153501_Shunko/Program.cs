e0cb70b166f7a7875ab516e1bc0b51877298356c
Victor <siva500000217@mail.ru>
2022-03-09 18:49:20 +0300

    Добавьте файлы проекта.

---
+++ _153501_Shunko/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _153501_Shunko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a / b;
+            Console.WriteLine("Result is {0}", c);
+        }
+    }
+}
